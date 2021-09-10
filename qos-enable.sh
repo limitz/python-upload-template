8c68220dabf14924fb23fef5f4b920d82da54197
E/S Pronk <eddy@autofilltech.com>
2021-09-10 10:12:50 -0400

    Now sets traffic control up during boot after network is up

--- qos-enable.sh
+++ qos-enable.sh
@@ -10,7 +10,7 @@ LOCAL=100mbit
 CIDR_LOCAL=11.0.2.0/24
 CIDR_UPSTR=0.0.0.0/0
 
-$TC qdisc del dev $IFACE root
+#$TC qdisc del dev $IFACE root
 
 $TC qdisc add dev $IFACE root handle 1:0 htb default 30
 $TC class add dev $IFACE parent 1:0 classid 1:1 htb rate $LIMIT
@@ -21,3 +21,4 @@ $TC class add dev $IFACE parent 1:1 classid 1:30 htb rate $START ceil $BWCAP
 $TC filter add dev $IFACE protocol ip parent 1:0 prio 1 u32 match ip dst $CIDR_LOCAL flowid 1:10
 $TC filter add dev $IFACE protocol ip parent 1:0 prio 2 u32 match ip dst $CIDR_UPSTR flowid 1:30
 
+echo "QoS should be enabled"
